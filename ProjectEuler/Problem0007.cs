171000af7bbeca2c408b0ff4d8730088df86c518
Andrew Planchak <andy.planchak@gmail.com>
2023-04-22 16:45:03 +0100

    Problem 7

---
+++ ProjectEuler/Problem0007.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    internal class Problem0007
+    {
+        public static void run()
+        {
+            int count = 0;
+            int n = 0;
+
+            while (count <= 10001)
+            {
+                n++;
+                if (UsefulMethods.isPrime(n) == true) count++;
+            }
+
+            Console.WriteLine(n);
+        } //COMPLETE
+    }
+}
