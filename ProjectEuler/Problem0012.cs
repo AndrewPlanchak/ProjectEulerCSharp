5fc9ea5917c24bab33a7cbb6a7a4889a2cb3cc12
Andrew Planchak <andy.planchak@gmail.com>
2023-04-22 16:56:46 +0100

    Problem 12

---
+++ ProjectEuler/Problem0012.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    internal class Problem0012
+    {
+        public static void run()
+        {
+            int numOfDivisors = 0;
+            int i = 0;
+            int triNum = 0;
+
+            while (numOfDivisors <= 500)
+            {
+                i++;
+                triNum += i;
+                int[] divisors = UsefulMethods.getDivisors(triNum);
+                if (divisors != null) numOfDivisors = divisors.Length;
+            }
+
+            Console.WriteLine(triNum);
+        } //COMPLETE  
+    }
+}
