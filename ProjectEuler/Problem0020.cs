4907f3582bb1367604453b9ff48c39d0edb49974
Andrew Planchak <andy.planchak@gmail.com>
2023-04-22 17:09:30 +0100

    Problem 20

---
+++ ProjectEuler/Problem0020.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    internal class Problem0020
+    {
+            public static void run()
+            {
+                BigInteger val = 1;
+
+                for (int i = 2; i <= 100; i++)
+                {
+                    val *= i;
+                }
+
+                int total = 0;
+
+                foreach (char digit in val.ToString())
+                {
+                    total += int.Parse(digit.ToString());
+                }
+
+                Console.WriteLine(total);
+            }
+    }
+}
