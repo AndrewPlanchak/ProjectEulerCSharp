9027227b5c8e1135747de7ff855ad73c5186c51a
Andrew Planchak <andy.planchak@gmail.com>
2023-04-22 17:09:01 +0100

    Problem 15

---
+++ ProjectEuler/Problem0015.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    internal class Problem0015
+    {
+        public static void run()
+        {
+            const int gridSize = 20;
+            long paths = 1;
+
+            for (int i = 0; i < gridSize; i++)
+            {
+                paths *= (2 * gridSize) - i;
+                paths /= i + 1;
+            }
+
+            Console.WriteLine(paths);
+        } //COMPLETE
+    }
+}
