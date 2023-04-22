0bc94481d94f856d9d3db44233d63d448b9bb520
Andrew Planchak <andy.planchak@gmail.com>
2023-04-22 16:44:26 +0100

    Problem 6

---
+++ ProjectEuler/Problem0006.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    internal class Problem0006
+    {
+            public static void run()
+            {
+                int sumOfSquares = 0;
+                int sum = 0;
+                for (int i = 0; i <= 100; i++)
+                {
+                    sumOfSquares += (i * i);
+                    sum += i;
+                }
+
+                int squareOfSums = sum * sum;
+
+                Console.WriteLine(squareOfSums - sumOfSquares);
+            } //COMPLETE
+    }
+}
