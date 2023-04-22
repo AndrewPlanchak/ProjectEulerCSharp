dae932f38a2505b8228f90c1fdd5083cfbc49746
Andrew Planchak <andy.planchak@gmail.com>
2023-04-22 17:09:59 +0100

    Problem 25

---
+++ ProjectEuler/Problem0025.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    internal class Problem0025
+    {
+        public static void run()
+        {
+            BigInteger a = 1;
+            BigInteger b = 1;
+            int index = 1;
+            int n = 1000;
+
+            while (a.ToString().Length < n)
+            {
+                BigInteger temp = b;
+                b = a + b;
+                a = temp;
+                index++;
+            }
+
+            Console.WriteLine(index);
+        }
+    }
+}
