b7e858322a66266a60e1826e86b178742c861463
Andrew Planchak <andy.planchak@gmail.com>
2023-04-22 16:57:08 +0100

    Problem 13

---
+++ ProjectEuler/Problem0013.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    internal class Problem0013
+    {
+        public static void run()
+        {
+            BigInteger i = 0;
+
+            StreamReader sr = new StreamReader(@"C:\Users\andyp\source\repos\Project Euler\Project Euler\problem0013.txt");
+
+            while (!sr.EndOfStream)
+            {
+                i += BigInteger.Parse(sr.ReadLine());
+            }
+
+            Console.WriteLine(i.ToString().Substring(0, 10));
+        } //COMPLETE
+    }
+}
