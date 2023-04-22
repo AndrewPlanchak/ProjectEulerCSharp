27991409300acb2abc543996659cd973dc0f9093
Andrew Planchak <andy.planchak@gmail.com>
2023-04-22 17:09:07 +0100

    Problem 16

---
+++ ProjectEuler/Problem0016.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    internal class Problem0016
+    {
+        public static void run()
+        {
+            BigInteger i = (BigInteger)Math.Pow(2, 1000);
+            string number = i.ToString();
+            int count = 0;
+            foreach (char c in number)
+            {
+                count += (int)c;
+            }
+            Console.WriteLine(count);
+        } //COMPLETE
+    }
+}
