a28f41700af3d287dbcae915a1f63ec732586715
Andrew Planchak <andy.planchak@gmail.com>
2023-04-22 17:09:49 +0100

    Problem 23

---
+++ ProjectEuler/Problem0023.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    internal class Problem0023
+    {
+        public static void run()
+        {
+            const int limit = 28123;
+
+            bool[] abundant = new bool[limit + 1];
+            int sum = 0;
+
+            for (int i = 1; i <= limit; i++) abundant[i] = UsefulMethods.GetDivisorSum(i) > i;
+
+            for (int i = 1; i <= limit; i++)
+            {
+                if (!IsSumOfTwoAbundant(i, abundant))
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+        public static bool IsSumOfTwoAbundant(int n, bool[] abundant)
+        {
+            for (int i = 1; i <= n / 2; i++)
+            {
+                if (abundant[i] && abundant[n - i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
